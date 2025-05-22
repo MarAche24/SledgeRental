279cdcd03bf9691fe1c09d9087049d0ad7b54066
Martin Achenrainer <martin.achenrainer@advokat.at>
2025-05-22 14:48:53 +0200

    finish

--- SledgeRental/Data/DesignTimeDbContextFactory.cs
+++ SledgeRental/Data/DesignTimeDbContextFactory.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using System.Diagnostics;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+//using System.Diagnostics;
 
-namespace SledgeRental.Data
-{
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//namespace SledgeRental.Data
+//{
+//    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+//    {
+//        public ApplicationDbContext CreateDbContext(string[] args)
+//        {
+//            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .Build();
+//            IConfiguration config = new ConfigurationBuilder()
+//                .SetBasePath(Directory.GetCurrentDirectory())
+//                .AddJsonFile("appsettings.json")
+//                .Build();
             
-            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+//            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
             
-            return new ApplicationDbContext(optionsBuilder.Options);
-        }
-    }
-}
+//            return new ApplicationDbContext(optionsBuilder.Options);
+//        }
+//    }
+//}
