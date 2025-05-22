03f7e420787730d0563e9e48f417598f926f27c5
Martin Achenrainer <martin.achenrainer@advokat.at>
2025-05-22 09:19:23 +0200

    Added design time DbContextFactory for migrations

---
+++ SledgeRental/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Diagnostics;
+
+namespace SledgeRental.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
